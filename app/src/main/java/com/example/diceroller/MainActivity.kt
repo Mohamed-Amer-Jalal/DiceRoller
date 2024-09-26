770561687daaeaf19cde50ed50030efd1f5e898c
Mohamed Amer <mohamedamerjalal@gmail.com>
2024-09-26 21:59:06 +0300

    DiceRoller 2nd

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -77,7 +78,7 @@ fun DiceWithButtonAndImage(
         Button(
             onClick = { result = (1..6).random() },
         ) {
-            Text(text = "Roll", fontSize = 24.sp)
+            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
     }
 }
